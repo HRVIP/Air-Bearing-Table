173a82f7201a05e82e55f21f02a76dfc910c9421
Natasha Evans <54152487+natasha0801@users.noreply.github.com>
2023-02-08 14:18:48 -0800

    Add files via upload

--- ArucoTracking/cameraPoseFromMarkerPose.py
+++ ArucoTracking/cameraPoseFromMarkerPose.py
@@ -1,37 +1,20 @@
 import cv2
 import numpy as np
 
-### Extracts camera pose from a single marker pose
+# Extracts camera pose from Aruco board pose
+# If the Aruco board origin is equal to the air table origin, then the resulting camera pose is relative to the table,
+# Reference: https://aliyasineser.medium.com/calculation-relative-positions-of-aruco-markers-eee9cc4036e3
 # Parameters:
-# rCamToMarker --> rotation vector of marker relative to camera (3x1)
-# tCamToMarker --> translation vector of marker relative to camera
-# rTableToMarker --> known rotation of marker relative to table (3x1)
-# tTableToMarker --> known position of marker relative to table
+# rCamToMarker --> rotation vector of board relative to camera (3x1)
+# tCamToMarker --> translation vector of board relative to camera
 # Returns:
-# rTableToCam --> camera "attitude" relative to table (3x1)
-# tTableToCam --> camera "position" relative to table
-def cameraPoseFromMarkerPose(rCamToMarker, tCamToMarker, rTableToMarker, tTableToMarker):
+# rMarkerToCam --> camera "attitude" relative to board (3x1)
+# tMarkerToCam --> camera "position" relative to board
+def cameraPoseFromMarkerPose(rCamToMarker, tCamToMarker):
 
-    #### Calculate camera position
-    tMarkerToCam = -1 * tCamToMarker            # camera position relative to marker
-    tTableToCam = tTableToMarker + tMarkerToCam # marker position relative to table
+    rCamToMarkerMatrix = cv2.Rodrigues(rCamToMarker)[0]     # convert rotation vector to matrix
+    rMarkerToCamMatrix = np.linalg.inv(rCamToMarkerMatrix)  # inverse of rotation matrix is rotation of camera relative to board
+    rMarkerToCam = cv2.Rodrigues(rMarkerToCamMatrix)[0]     # camera rotation as a vector
+    tMarkerToCam = np.dot(rMarkerToCamMatrix, np.matrix(-1 * tCamToMarker))     # camera position relative to marker/board
+    return rMarkerToCam, tMarkerToCam
 
-    ### Calculate camera rotation
-
-    # Convert everything to rotation matrices (easier to work with, unfortunately)
-    rTableToMarkerMatrix = cv2.Rodrigues(rTableToMarker)[0]     # marker rotation relative to table as matrix
-    rCamToMarkerMatrix = cv2.Rodrigues(rCamToMarker)[0]         # marker rotation relative to camera as matrix
-    rMarkerToCamMatrix = np.linalg.inv(rCamToMarkerMatrix)      # camera rotation relative to marker as matrix
-    rTableToCamMatrix = np.matmul(rTableToMarkerMatrix, rMarkerToCamMatrix) # camera rotation relative to table as matrix
-    rTableToCam = cv2.Rodrigues(rTableToCamMatrix)[0]   # camera rotation relative to table as vector
-
-    return rTableToCam, tTableToCam
-
-# test
-rCamToMarker = np.array([0, 0, 5 * np.pi / 4])
-rTableToMarker = np.array([0, 0, np.pi])
-tCamToMarker = np.array([-1, 2, 0])
-tTableToMarker = np.array([1, 3, 0])
-r, t = cameraPoseFromMarkerPose(rCamToMarker, tCamToMarker, rTableToMarker, tTableToMarker)
-print(r)
-print(t)
