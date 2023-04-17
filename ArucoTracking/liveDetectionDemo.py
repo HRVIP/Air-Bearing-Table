7612bd02857e2d938ef4a8185c0091bef302f5a4
natasha0801 <natashahevans@yahoo.com>
2023-04-17 14:49:38 -0700

    live detection demo for lab reunion

--- ArucoTracking/liveDetectionDemo.py
+++ ArucoTracking/liveDetectionDemo.py
@@ -1,4 +1,4 @@
-# Pi Camera real-time video marker detection - for lab reuinion demo!
+# Pi Camera real-time video marker detection - for lab reunion demo!
 
 
 ### Imports
@@ -18,20 +18,18 @@ marker_side_length = 0.040       				# marker side length in m
 calibLoc = '../images/calibImages/calib_april_11_low_res.yaml'   # calibration file location
 width = int(640)               					# output image width (px)
 height = int(480)              					# output image height (px)
-framerate = 32									# camera framerate
+framerate = 40									# camera framerate
 
 ### Import calibration parameters
-print("Import calibration parameters...")
+print("Welcome to live marker detection!")
+print("Press q to quit at any time.")
+print("This video will time out after 10 minutes; press the green arrow at the top to restart.")
+print("The camera will detect and mark axes on the ArUco marker (next to keyboard!)")
 calibFile = cv2.FileStorage(calibLoc, cv2.FILE_STORAGE_READ)    # load in camera calibration file
 cameraMatrix = calibFile.getNode("camera_matrix").mat()         # camera calibration matrix
 distCoeffs = calibFile.getNode("dist_coeff").mat()              # camera distortion matrix
-print("Camera Matrix: ")
-print(cameraMatrix)
-print("Distortion Coefficients: ")
-print(distCoeffs)
 
 ### Camera setup
-print("Setting up camera...")
 camera = PiCamera()
 camera.resolution = (width, height)						# TODO: determine relationship between resolution and runtime (tradeoff with accuracy?)
 camera.framerate = framerate							# frames per second
@@ -39,7 +37,6 @@ rawCapture = PiRGBArray(camera, size=(width, height))	# 3D RGB array
 time.sleep(1)											# allow camera to warm up
 
 ### Set up Aruco detector
-print("Setting up Aruco parameters...")
 arucoDict = cv2.aruco.getPredefinedDictionary(0)		# small dictionary of 4x4 markers
 arucoParams = cv2.aruco.DetectorParameters_create()     # default aruco parameters
 
@@ -63,7 +60,7 @@ board = aruco.Board_create(boardCorners, arucoDict, boardIDs)	# actual board obj
 ### For demo, we don't need output files or multiprocessing (yay!)
 
 # TODO: exit condition (user press q) doesn't seem to be working, so for now, we use a temporary exit condition of running for a certain time
-runtime = 60
+runtime = 600 # just in case exit code fails :)
 startTime = time.time()
 exampleText="Position: X=-0.00, Y=-0.00, Z=-0.00"
 labelCoordsTop = (20, 50)
@@ -79,7 +76,6 @@ xt, yt = labelCoordsTop
 xb, yb = labelCoordsBottom
 cv2.namedWindow('video', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('video', 640, 480)
-print("Starting capture...")
 for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     
     frame = image.array			# capture frame as an array
@@ -87,8 +83,12 @@ for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)   # convert to grayscale
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, arucoDict, parameters=arucoParams, cameraMatrix=cameraMatrix, distCoeff=distCoeffs) # marker detection
     ret, rvec, tvec = aruco.estimatePoseBoard(corners, ids, board, cameraMatrix, distCoeffs, rvec_init, tvec_init)	# estimate board pose using markers
-    labelTextTop = "Position: X={:.2f}, Y={:.2f}, Z={:.2f} ".format(tvec[0], tvec[1], tvec[2])
-    labelTextBottom =  "Rotation: X={:.2f}, Y={:.2f}, Z={:.2f}".format(rvec[0], rvec[1], rvec[2])
+    if (len(corners) != 0):
+        labelTextTop = "Position: X={:.2f}, Y={:.2f}, Z={:.2f} ".format(tvec[0], tvec[1], tvec[2])
+        labelTextBottom =  "Rotation: X={:.2f}, Y={:.2f}, Z={:.2f}".format(rvec[0], rvec[1], rvec[2])
+    else:
+        labelTextTop = exampleText
+        labelTextBottom = exampleText
     capWithAxes = cv2.drawFrameAxes(frame, cameraMatrix, distCoeffs, rvec, tvec, 0.1)		# real-time visualization: draw axes
     cv2.rectangle(capWithAxes, labelCoordsTop, (xt + text_w, yt - text_h), bgColor, -1)
     cv2.rectangle(capWithAxes, labelCoordsBottom,(xb + text_w, yb - text_h), bgColor, -1)
@@ -99,13 +99,11 @@ for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     
     # Stop video if user quits
     if cv2.waitKey(1) & 0xFF == ord('q') or time.time() - startTime > runtime:                 
-        print("Stopping all recording.")
         break
 
 # Cleanup
 print("Cleaning up...")
 cv2.destroyAllWindows()
-print("Stopping marker detection.")
 time.sleep(1)
 cv2.destroyAllWindows()
 print("Done!")
